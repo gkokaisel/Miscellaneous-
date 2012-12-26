b1dcac06c5fa0ec4f14ac357b72e28ce46250dff
gkokaisel <gkokaisel@gmail.com>
2012-12-26 10:44:29 -0500

    Update respondus_converter.py

--- respondus_converter.py
+++ respondus_converter.py
@@ -5,122 +5,104 @@ Created on Dec 21, 2012
 Program for converting tests to Respondus format
 version 1.5
 @author: Gary Kokaisel
+
+Resources:
+Regular expression operations, http://docs.python.org/2/library/re.html
+Python regular expressions, http://www.tutorialspoint.com/python/python_reg_expressions.htm
+Regular Expressions Syntax Referance, http://www.regular-expressions.info/reference.html
+Online pythonic regex tool http://re-try.appspot.com/
+Easy GUI tutorial, http://www.ferg.org/easygui/tutorial.html#contents_item_9.2
 '''
 
 import sys
+import string
 import zipfile, re
-import easygui  # http://www.ferg.org/easygui/tutorial.html#contents_item_9.2
+import easygui
 
-# regular expressions to find test_file answers (very useful pythonic regex tool online http://re-try.appspot.com/)
+answer_regex = \
+r'''
 
-answer_regexes = \
-    r'''
-            
-(^Answer|\nAnswer|Answer:|ANSWER:|ANS:)(.*)                            # find answer key in its many variations                                        
-                                        
+(^Answer|\nAnswer|Answer:|ANSWER:|ANS:)(.*)                            # find answer key in its many variations
 
-            '''
-feedback_regexes = \
+'''
+true_false_regex = \
+r'''
+
+(Answer:\s+True)|(Answer:\s+False)                                    # find True and False type
+|(Answer\s+True)|(Answer\s+False)
+|(ANS:\s+True)|(ANS:\s+False)
+
+'''
+feedback_regex = \
 r'''
 
 (Diff:.*|Topic:.*|Skill:.*|Geog\sStandards:.*                          # find common feedback terms...
-|Bloom's\sTaxonomy:.*|AACSB:.*|Question\sStatus:.*)                    # Diff: 3  
+|Bloom's\sTaxonomy:.*|AACSB:.*|Question\sStatus:.*)                    # Diff: 3
                                                                        # Topic:  Shape of Earth
                                                                        # Geog Standards:  New Geog Standards 4
                                                                        # Bloom's Taxonomy:  Knowledge
 
 '''
-statistical_regexes = \
+stats_regex = \
 r'''
 
 (%.*)                                                                  # removes any stastical type of feedback from test...
-                                                                       # % correct 60      a= 60  b= 7  c= 18  d= 16      r = .21   
-                                                                                                                                                
+                                                                       # % correct 60      a= 60  b= 7  c= 18  d= 16      r = .21
+
 '''
-
-# helper function to distinquish between test questions and test answer key 
-def process_test(test):      
-  
-    # re.I ignores letter case, and re.X ignores comments and whitespace (unless included in pattern) within regular expression      
-    answer_match = re.findall(answer_regexes, test, flags=re.I | re.X)
- 
-    if answer_match:   
-        # list comprehension to return just the letter answer within tuple
-        answer_key = [x[1] for x in answer_match]
-        
-        # sustitute answers with empty string   
-        clean_test = re.sub(answer_regexes, '', test, flags=re.I | re.X)      
-        print re.sub(feedback_regexes, '', clean_test, flags=re.I | re.X)   
-        format_answers(answer_key)
-         
-# helper function to format answer key as ordered list  
-def format_answers(answers):    
-     
-    # the following code will print answer key as a numerically ordered list of answers in Respondus format  
-    print 'Answers:'
-    number = 0
-    for answer in answers:       
-        number += 1    
-        # append ordered numbers to answer key, remove any stray colons   
-        answers = str(number) + '.' + answer.replace(":", "")
-        
-        # remove any stastical type feedback from answer key
-        print re.sub(statistical_regexes, "", answers, flags=re.I | re.X)
-
-# main function to launch program dialog, and to set input and output file paths
-def main():
+def main():     
+    def process_test(test):
+        answer_match = string_find(answer_regex, test)
+        if answer_match:
+            answer_key = [x[1] for x in answer_match]
+            test = string_replace(true_false_regex, "A) True\nB) False\n", test)
+            test = string_replace(answer_regex, '', test)
+            test = string_replace(feedback_regex, '', test)
+            print test
+            format_answers(answer_key)
     
-    # load GUI to begin program dialog   
+    def string_replace(pattern, string_old, string_new):
+        return re.sub(pattern, string_old, string_new, flags=re.I | re.X)
+    
+    def string_find(pattern, string):
+        return re.findall(pattern, string, flags=re.I | re.X)
+    
+    def format_answers(answers):
+        print 'Answers:'
+        number = 0
+        for answer in answers:
+            number += 1
+            answers = str(number) + '.' + answer.replace(":", "")
+            answers = string_replace(stats_regex, "", answers)
+            print string.capwords(answers)    
+    
     msg = "This program will attempt to format a test file for use with Respondus, Do you want to continue?"
     title = "Respondus Format Utility version 1.5 Beta"
-    if easygui.ccbox(msg, title):  # show a Continue/Cancel dialog
-        pass  # user chose Continue
-    else:  # user chose Cancel
+    if easygui.ccbox(msg, title):
+        pass
+    else:
         sys.exit(0)
-    
-    # launch GUI to choose location for output file    
-    save_output = sys.stdout
-    output_file = open(easygui.filesavebox(msg='Before we begin choose where to save your formatted test file?',
-                   default='formatted_test.txt'), 'w')
-    sys.stdout = output_file
-    
-    # launch GUI to load test file, and store as string (replacing singles lines with double lines to make matching regexes easier)    
+    sys.stdout = open(easygui.filesavebox(msg='Before we begin choose where to save your formatted test file?',
+                   default='formatted_test.txt'), 'w')    
     file_choice = easygui.indexbox(msg="Choose the file type of your test file (Note: Word is very experimental)",
                                    choices=("Plain text file (.txt)", "Word 2007, 2010 file (.docx)", "Quit"))
-    
-    if file_choice is 0:   
-        
-        # launch GUI to load test file     
+    if file_choice is 0:
         input_file = easygui.fileopenbox(msg='Where is the test file to format?')
         try:
             with open(input_file) as inputFileHandle:
-                test_file = inputFileHandle.read().replace('\n', '\n\n')
-                process_test(test_file)
+                process_test(inputFileHandle.read().replace("\n", "\n\n"))            
         except IOError:
             sys.stderr.write('Could not open %s\n' % input_file)
-            sys.exit(-1)  
-        
-    elif file_choice is 1: 
-        
-        # launch GUI to load document and unzip files       
+            sys.exit(-1)
+    elif file_choice is 1:
         docx = zipfile.ZipFile(easygui.fileopenbox(msg='Where is the test file to format?'))
-        
-        # load xml document from word directory
         content = docx.read('word/document.xml')
-        
-        # substitute xml tags with line breaks (still needs tweaking, but sorta works)     
-        test_file = re.sub('<(.|\n)*?>', '\n', content)
-
-        process_test(test_file)
+        process_test(re.sub('<(.|\n)*?>', '\n', content))    
     else:
-        sys.exit(0)  
-     
-    # load GUI to display success message
-    easygui.msgbox("Format complete!", ok_button="Close", image="tick_64.png")   
-    # flush and close output
-    output_file.flush()
-    output_file.close()
-    sys.stdout = save_output            
+        sys.exit(0)
+    easygui.msgbox("Format complete!", ok_button="Close", image="tick_64.png")    
+    sys.stdout.flush()
+    sys.stdout.close()
 
 if __name__ == "__main__":
     main()
